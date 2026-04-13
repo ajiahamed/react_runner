bb3349e9380f54c0d051a2fe60bbca190acc3575
Aji Ahamed <ajiahamed@gmail.com>
2026-04-13 12:23:17 +0530

    Updated the APP.js file

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ function App() {
     <div style={{ textAlign: "center", marginTop: "50px" }}>
       <h1>🚀 Docker Multi-Stage Frontend</h1>
       <p>Served using Nginx</p>
+      <p> CICD using GitHub Action selfhosted</p>
     </div>
   );
 }
