0c3fb8c08bdcf178f72784ca5ec78a5fbe57491b
Aji Ahamed <ajiahamed@gmail.com>
2026-04-13 07:43:00 +0530

    My new and initial commit for runner testing

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>🚀 Docker Multi-Stage Frontend</h1>
+      <p>Served using Nginx</p>
+    </div>
+  );
+}
+
+export default App;
